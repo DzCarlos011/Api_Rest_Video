167e74fa27187574c306c076beff32d3a31889a9
DzCarlos011 <anonimoxo1908@gmail.com>
2023-11-05 21:18:51 -0700

    documento subido

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+
+import cors from 'cors';
+import express from 'express';
+
+import db from './config/mongo';
+import { router } from './routes';
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+db().then(() => console.log("Conexion Ready"));
+app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
